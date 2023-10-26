9d921889349cfc343afa112af5e87b5f379b9a98
Artem <artemwot90@gmail.com>
2023-10-26 16:59:44 +0300

    Update comment.service.js

--- server/src/services/comment.service.js
+++ server/src/services/comment.service.js
@@ -1,5 +1,35 @@
 import sequelize from '../database/db.connection.js';
 
 export default class CommentService {
+    async selectAll(data) {
 
+    }
+
+    async selectAllHead(data) {
+
+    }
+    
+    async selectAllNested(data) {
+
+    }
+    
+    async selectById(data) {
+
+    }
+
+    async selectAllNestedByHeadId(data) {
+
+    }
+    
+    async create(data) {
+
+    }
+
+    async update(data){
+
+    }
+
+    async deleteById(data) {
+
+    }
 }
